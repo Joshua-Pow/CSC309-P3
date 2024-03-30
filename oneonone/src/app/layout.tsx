ff358ec4dfe311a75845bcf6e27e6be3d0bc0eba
Joshua Pow <jokesjosh@gmail.com>
2024-03-30 18:34:52 -0400

    installed shadcn, nextui and added navbar

--- oneonone/src/app/layout.tsx
+++ oneonone/src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import CustomNavbar from "@/app/components/CustomNavbar";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +21,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers>
+          <CustomNavbar />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
