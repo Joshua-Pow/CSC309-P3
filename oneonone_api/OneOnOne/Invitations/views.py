9a89c6a93773763e0b4a96fac732e0c7f3c02dd0
Joshua Pow <jokesjosh@gmail.com>
2024-04-08 00:05:58 -0400

    updated get for /calendar/<id>/invitations/ to return latest invite status for each of the users friends

--- oneonone_api/OneOnOne/Invitations/views.py
+++ oneonone_api/OneOnOne/Invitations/views.py
@@ -16,6 +16,8 @@ from drf_spectacular.utils import (
     extend_schema_view,
 )
 from Calendars.models import Participant
+from Contacts.models import Contact
+from rest_framework.response import Response
 
 
 @extend_schema_view(
@@ -43,6 +45,56 @@ class InvitationListCreateAPIView(generics.ListCreateAPIView):
                 "You do not have permission to view these invitations."
             )
         return Invitation.objects.filter(calendar=calendar)
+
+    def list(self, request, *args, **kwargs):
+        calendar_id = self.kwargs.get("calendar_id")
+        calendar = get_object_or_404(Calendar, id=calendar_id)
+        if calendar.creator != request.user:
+            raise PermissionDenied(
+                "You do not have permission to view these invitations."
+            )
+
+        # Fetch all contacts where the current user is either userA or userB and status is friends (integer 2)
+        contacts = Contact.objects.filter(
+            (Q(userA=request.user) | Q(userB=request.user)) & Q(status="1")
+        )
+        print("contacts", contacts)
+
+        # Prepare a list to hold the response data
+        data = []
+
+        for contact in contacts:
+            # Determine the username of the contact that is not the current user
+            contact_user = (
+                contact.userB if contact.userA == request.user else contact.userA
+            )
+
+            # Fetch the most recent invitation for the contact for the specified calendar
+            latest_invitation = (
+                Invitation.objects.filter(calendar_id=calendar_id, invitee=contact_user)
+                .order_by("-updated_at")
+                .first()
+            )
+
+            # Prepare the contact data, including the latest invitation status if available
+            contact_data = {
+                "calendar_id": calendar_id,
+                "username": contact_user.username,
+                "firstName": contact_user.first_name,
+                "lastName": contact_user.last_name,
+                "email": (
+                    contact.userB.email
+                    if contact.userA == request.user
+                    else contact.userA.email
+                ),  # Assuming both userA and userB have an email field
+                "status": (
+                    latest_invitation.status if latest_invitation else "notInvited"
+                ),
+            }
+
+            data.append(contact_data)
+
+        return Response(data)
 
     def perform_create(self, serializer):
         calendar_id = self.kwargs.get("calendar_id")
